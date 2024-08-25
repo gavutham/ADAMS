ddb34c0e81a9c4fee5818c38d87bce6afec886f2
gavutham <kgavutham2005@gmail.com>
2024-08-25 21:59:14 +0530

    disable button on click

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,173 @@
-import 'package:adams/models/student.dart';
-import 'package:adams/screens/wrapper.dart';
-import 'package:adams/services/auth.dart';
-import 'package:adams/services/database.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import "package:adams/models/student.dart";
+import "package:adams/screens/statistics/statistics.dart";
+import "package:adams/services/auth.dart";
+import "package:adams/services/database.dart";
+import "package:adams/shared/loading.dart";
+import 'package:adams/services/bluetooth.dart';
+import 'package:adams/services/server.dart';
+import "package:firebase_database/firebase_database.dart";
+import "package:flutter/material.dart";
+import "package:provider/provider.dart";
+import "../../authenticate_face/authenticate_face_view.dart";
 
-import 'common/utils/custom_snackbar.dart';
-import 'common/utils/screen_size_util.dart';
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+  @override
+  State<Home> createState() => _HomeState();
 }
 
-void initializeUtilContexts(BuildContext context) {
-  ScreenSizeUtil.context = context;
-  CustomSnackBar.context = context;
-}
+class _HomeState extends State<Home> {
+  final AuthService _auth = AuthService();
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
+    final student = Provider.of<StudentData?>(context);
 
-    initializeUtilContexts(context);
-    return StreamBuilder<StudentUser?>(
-      stream: AuthService().user,
-      builder: (context, snapshot) {
-        final user = snapshot.hasData ? snapshot.data : null;
-        return StreamBuilder<StudentData?>(
-          initialData: null,
-          stream: DatabaseService(sid: user?.sid).studentData,
-          builder: (context, snapshot) {
-            return MultiProvider(
-              providers: [
-                StreamProvider<StudentUser?>.value(
-                  value: AuthService().user,
-                  initialData: null,
-                ),
-                StreamProvider<StudentData?>.value(
-                  initialData: null,
-                  value: DatabaseService(sid: user?.sid).studentData,
-                )
-              ],
-              child: const MaterialApp(
-                home: Wrapper(),
-                debugShowCheckedModeBanner: false,
-              ),
-            );
-          }
+    DatabaseReference portalStateRef = FirebaseDatabase.instance
+        .ref("${student?.year}/${student?.department}/${student?.section}");
+    final db = DatabaseService(sid: student?.sid);
+
+    handleSubmit() async {
+
+      setState(() {
+        isLoading = true;
+      });
+
+      if (student != null) {
+        var response = false;
+
+        var uuid = await getUuid(student); // getsUuid of the session (bf27730d-860a-4e09-889c-2d8b6a9e0fe7)
+        print(uuid);
+        turnOn(); //turn on bluetooth
+
+        while (!response) {
+          await advertise(uuid);
+          response = await verify(student);
+        }
+
+        //need to fix the flow (face auth)
+        // Navigator.of(context).push(
+        //   MaterialPageRoute(
+        //     builder: (context) => AuthenticateFaceView(student: student, date: date, interval: interval),
+        //   ),
+        // );
+
+
+        var nearbyDevices = await getDevices();
+        await postNearbyDevices(nearbyDevices, student);
+
+        const snackBar = SnackBar(
+          content: Text('Your attendance request has been received'),
         );
-      },
-    );
+
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      }
+
+      setState(() {
+        isLoading = false;
+      });
+
+    }
+
+    if (student != null) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("ADAMS"),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                _auth.signOut();
+              },
+              child: const Text("Logout"),
+            )
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Welcome,",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                      Text(
+                        student.name,
+                        style: const TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ],
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const Statistics()));
+                    },
+                    child: const Row(
+                      children: [
+                        Text(
+                          "See Stats",
+                          style: TextStyle(
+                            fontSize: 20,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          size: 15,
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 50,),
+              StreamBuilder(
+                stream: portalStateRef.onValue,
+                builder: (context, snapshot) {
+                  final portalOpen = snapshot.data != null
+                      ? snapshot.data!.snapshot.value as bool
+                      : false;
+                  print(portalOpen);
+                  return Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: portalOpen ? isLoading ? null : handleSubmit : null,
+                        child: const Text("Mark attendance"),
+
+                      ),
+                      const SizedBox(height: 10,),
+                      Text(
+                        //get current attendance session from mongo
+                        portalOpen ? "There is an active attendance session" : "No Active Attendance Session",
+                        style: const TextStyle(
+                          color: Colors.black87,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      )
+                    ],
+                  );
+                },
+              )
+            ],
+          ),
+        ),
+      );
+    } else {
+      return const Loading();
+    }
   }
 }
-
